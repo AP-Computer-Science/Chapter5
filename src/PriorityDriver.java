304614652a0e86a02a015707e11e16aeb1008819
Brian Barnes <brian.jx.b@gmail.com>
2018-12-19 14:36:51 -0600

    Finished with horrible sorting.

--- src/PriorityDriver.java
+++ src/PriorityDriver.java
@@ -1,11 +1,96 @@
+import java.util.Scanner;
+
 public class PriorityDriver {
     public static void main(String[] args) {
-        Task t1 = new Task();
-        Task t2 = new Task();
-        Task t3 = new Task();
-        t1.setPriority(3);
-        t2.setPriority(1);
-        t3.setPriority(2);
-        
+        Scanner s = new Scanner(System.in);
+        Task t1 = null;
+        Task t2 = null;
+        Task t3 = null;
+        System.out.print("Enter task name for t1:");
+        t1 = new Task();
+        System.out.print("Enter priority for t1:");
+        t1.setPriority(s.nextInt());
+        System.out.print("Enter complexity for t1:");
+        t1.setComplexity(s.nextInt());
+        System.out.print("Enter task name for t2:");
+        t2 = new Task();
+        System.out.print("Enter priority for t2:");
+        t2.setPriority(s.nextInt());
+        System.out.print("Enter complexity for t2:");
+        t2.setComplexity(s.nextInt());
+        System.out.print("Enter task name for t3:");
+        t3 = new Task();
+        System.out.print("Enter priority for t3:");
+        t3.setPriority(s.nextInt());
+        System.out.print("Enter complexity for t3:");
+        t3.setComplexity(s.nextInt());
+        System.out.println("Ordered by priority:");
+        if(t1.compareTo(t2) >= 1 && t1.compareTo(t3) >= 1) {
+            System.out.println(t1);
+            if(t2.compareTo(t3) >= 1) {
+                System.out.println(t2);
+                System.out.println(t3);
+            }
+            else {
+                System.out.println(t3);
+                System.out.println(t2);
+            }
+        }
+        else if(t2.compareTo(t1) >= 1 && t2.compareTo(t3) >= 1) {
+            System.out.println(t2);
+            if(t1.compareTo(t3) >= 1) {
+                System.out.println(t1);
+                System.out.println(t3);
+            }
+            else {
+                System.out.println(t3);
+                System.out.println(t1);
+            }
+        }
+        else if(t3.compareTo(t1) >= 1 && t3.compareTo(t2) >= 1) {
+            System.out.println(t3);
+            if(t1.compareTo(t2) >= 1) {
+                System.out.println(t1);
+                System.out.println(t2);
+            }
+            else {
+                System.out.println(t2);
+                System.out.println(t1);
+            }
+        }
+        System.out.println("Ordered by complexity.");
+        if((t1.compareTo(t2) == 0 || t1.compareTo(t2) == 2) && (t1.compareTo(t3) == 0 || t1.compareTo(t3) == 2)) {
+            System.out.println(t1);
+            if(t2.compareTo(t3) == 0 || t2.compareTo(t3) == 2) {
+                System.out.println(t2);
+                System.out.println(t3);
+            }
+            else {
+                System.out.println(t3);
+                System.out.println(t2);
+            }
+        }
+        else if((t2.compareTo(t1) == 0 || t2.compareTo(t1) == 2) && (t2.compareTo(t3) == 0 || t2.compareTo(t3) == 2)) {
+            System.out.println(t2);
+            if(t1.compareTo(t3) == 0 || t1.compareTo(t3) == 2) {
+                System.out.println(t1);
+                System.out.println(t3);
+            }
+            else {
+                System.out.println(t3);
+                System.out.println(t1);
+            }
+        }
+        else if((t3.compareTo(t1) == 0 || t3.compareTo(t1) == 2) && (t3.compareTo(t2) == 0 || t3.compareTo(t2) == 2)) {
+            System.out.println(t3);
+            if(t1.compareTo(t2) == 0 || t1.compareTo(t2) == 2) {
+                System.out.println(t1);
+                System.out.println(t2);
+            }
+            else {
+                System.out.println(t2);
+                System.out.println(t1);
+            }
+        }
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,11 +1,62 @@
-public class Task implements PriorityInterface{
+
+import java.util.Scanner;
+
+public class Task implements PriorityInterface, Complexity, Comparable{
 
     private int _priority = 0;
+    private int _complexity = 0;
+    private String _taskName;
+    private Scanner _scan;
+    public Task() {
+        _scan = new Scanner(System.in);
+        _taskName = _scan.nextLine();
+    }
     public int getPriority() {
         return _priority;
     }
 
     public void setPriority(int pri) {
         _priority = pri;
+    }
+    public String toString() {
+        return _priority + ":" + _complexity + ": " + _taskName;
+    }
+
+    public void setComplexity(int complexity) {
+        _complexity = complexity;
+    }
+
+    public int getComplexity() {
+        return _complexity;
+    }
+
+    public int compareTo(Object t) {
+        /*
+        If this._priority is higher, add 1, else add -1;
+        If this._complexity is higher, add 1, else add 0;
+        Key:
+        2: Both more complex and higher priority.
+        1: Priority is higher, but complexity is lower.
+        0: Priority is not higher, but complexity is higher.
+        -1: Priority is not higher, and complexity is not higher.
+        */
+        Task newObject = (Task)t;
+        int priorityDifference = this.getPriority() - newObject.getPriority();
+        int complexityDifference = this.getComplexity() - newObject.getComplexity();
+        int result = 0;
+        if(priorityDifference > 0) {
+            result += 1;
+        }
+        else {
+            result += -1;
+        }
+        if(complexityDifference > 0) {
+            result += 1;
+        }
+        else {
+            result += 0;
+        }
+        //System.out.println(result);
+        return result;
     }
 }
